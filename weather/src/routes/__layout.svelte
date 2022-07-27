34e3b8759038eb2a4bf37b34494900daeab67e5c
JorgeGonzalezPascual <jorge.gonzalez@uib.es>
2022-07-27 20:25:29 +0200

    init

---
+++ weather/src/routes/__layout.svelte
@@ -0,0 +1,28 @@
+<div>
+    <slot />
+</div>
+
+<style>
+	:global(body) {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		background-color: #fdfdfd;
+		display: grid;
+		place-content: center;
+        min-height: 100vh;
+	}
+
+    :global(h1,h2,h3,h4,h5){
+        margin: 0;
+    }
+
+    div{
+        aspect-ratio: 9/16;
+        width: 400px;
+        max-width: 100%;
+        border: 1px solid #eee;
+        display: block;
+        box-shadow: 0 0 100px rgba(0,0,0,0.1);
+        border-radius: 8px;
+    }
+</style>
---
+++ weather/src/routes/index.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { getWeatherFrom } from './services/weather.js';
+	const weatherPromise = getWeatherFrom();
+</script>
+
+{#await weatherPromise then weather}
+	<!-- {console.log(weather)} -->
+	<section>
+		<h1>{weather.locationName}</h1>
+		<h2>{weather.temperature}°</h2>
+		<h3>{weather.conditionText}</h3>
+		<h4>{weather.windSpeed}</h4>
+	</section>
+{/await}
+
+<style>
+	
+	h1 {
+		/* font-size: 2em;
+		font-weight: bold;
+		text-align: center; */
+		font-weight: 300;
+		color: #333;
+		text-transform: uppercase;
+		padding: 32px 0 16px 16px;
+	}
+
+	h2 {
+		font-weight: 300;
+		font-size: 64px;
+		color: #333;
+		text-transform: uppercase;
+		padding: 32px 0 0px 0px;
+	}
+
+	h3 {
+		font-weight: 400;
+		transform: rotate(-90deg);
+		position: absolute;
+		top: 12px;
+		right: 12px;
+	}
+</style>
